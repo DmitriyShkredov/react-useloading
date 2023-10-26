e65175f13db62038d08a40ea6c7e5eb602928b08
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-10-26 16:44:44 +0300

    feat: fix log

--- src/useloading.js
+++ src/useloading.js
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 
 export const useLoading = (state) => {
   useEffect(() => {
-    console.log("--");
     if (state) document.body.classList.add("loading");
     else {
       document.body.classList.remove("loading");
